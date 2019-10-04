be651a1be6f24b4ac38ed4ec306a980d5e63fd1c
HappDen <53235180+HappDen@users.noreply.github.com>
2019-10-04 18:51:16 +0300

    Update zadanie5.py

--- zadanie5.py
+++ zadanie5.py
@@ -1,10 +1,10 @@
-    
 import math
 a = int(input())
 b = int(input())
 c = int(input())
 discr = b**2 - 4*a*c
-if a and b ==0 :
+
+if a==0 and b==0:
     print(" ")
 else:
     if discr > 0:
@@ -15,9 +15,8 @@ else:
             x1 = (-b + math.sqrt(discr))/ (2*a)
             x2 = (-b - math.sqrt(discr))/ (2*a) 
             print("x1:%.2f \nx2:%.2f" % (x1, x2))
-
     elif discr == 0:
         x = -b / (2*a)
         print("x1=:%.2f" % x)
     else:
-        print(" ")
+            print(" ")
