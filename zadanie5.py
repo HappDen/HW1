7cba4941f710dbde896b9e4aa90028b60af745da
HappDen <53235180+HappDen@users.noreply.github.com>
2019-10-04 17:53:01 +0300

    Update zadanie5.py

--- zadanie5.py
+++ zadanie5.py
@@ -3,18 +3,20 @@ a = int(input())
 b = int(input())
 c = int(input())
 discr = b**2 - 4*a*c
-
-if discr > 0:
-    if a == 0:
-        x1 = (c/b)
-        print("x1:-%.2f" % x1)
-    else:
-        x1 = (-b + math.sqrt(discr))/ (2*a)
-        x2 = (-b - math.sqrt(discr))/ (2*a) 
-        print("x1 = %.2f \nx2 = %.2f" % (x1, x2))
-
-elif discr == 0:
-    x = -b / (2*a)
-    print("x = %.2f" % x)
+if (a and b) ==0 :
+    print("Нет решений")
 else:
-    print(" ")
+    if discr >= 0:
+        if a == 0:
+            x1 = (c/b)
+            print("x1:-%.2f" % x1)
+        else:
+            x1 = (-b + math.sqrt(discr))/ (2*a)
+            x2 = (-b - math.sqrt(discr))/ (2*a) 
+            print("x1=%.2f \nx2=%.2f" % (x1, x2))
+
+    elif discr == 0:
+        x = -b / (2*a)
+        print("x = %.2f" % x)
+    else:
+        print(" ")
