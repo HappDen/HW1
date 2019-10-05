e15c1abc1759a89d4ae2a96424f941f156585122
HappDen <53235180+HappDen@users.noreply.github.com>
2019-10-05 07:57:42 +0300

    Update zadanie3.py

--- zadanie3.py
+++ zadanie3.py
@@ -2,10 +2,10 @@ a = int(input())
 b = int(input())
 c = int(input())
 import math
-if (a or b or c) < 10 :
-    d = math.sqrt(a**2+b**2)
-else:
+if a > b and a > c:
     d = math.sqrt(a**2-b**2)
+else:
+    d = math.sqrt(a**2+b**2)
 if d == c:
     print("YES")
 else:
