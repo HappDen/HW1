c9d75212aa44ff259854dbcb4d5e5cb0ead3ac8b
HappDen <53235180+HappDen@users.noreply.github.com>
2019-10-05 09:18:09 +0300

    Update zadanie5.py

--- zadanie5.py
+++ zadanie5.py
@@ -10,13 +10,13 @@ else:
     if discr > 0:
         if a == 0:
             x1 = (c/-b)
-            print("x1:%.2f" % x1)
+            print("X1:%.2f" % x1)
         else:
             x1 = (-b + math.sqrt(discr))/ (2*a)
             x2 = (-b - math.sqrt(discr))/ (2*a) 
-            print("x1:%.2f \nx2:%.2f" % (x1, x2))
+            print("X1:%.2f \nX2:%.2f" % (x1, x2))
     elif discr == 0:
         x = -b / (2*a)
-        print("x1=:%.2f" % x)
+        print("X1=:%.2f" % x)
     else:
         print(" ")
